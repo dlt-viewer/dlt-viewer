4221c982176580d3fdfdd4451fdf31fedff66fde
Hannes Achleitner <hannes.software@gmx.at>
2021-03-30 06:20:14 +0200

    Qt 6

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 if [[ "$(uname -s)" == 'Darwin' ]]; then
-    brew link qt@5 --force
+    brew link qt@6 --force
 else
     sudo apt-get update
-    sudo apt-get install -y build-essential qt5-default libqt5serialport5-dev
+    sudo apt-get install -y build-essential qt6-default libqt6serialport6-dev
 fi
